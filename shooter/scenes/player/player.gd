6d1516b875c9fa6224d5eaaaaf3ab43f1c5ecb54
BM-Haoh <joao.massabki@gmail.com>
2025-12-31 17:07:55 -0300

    Starting tracking of Godot Study (2D and 3D): 2D

---
+++ shooter/scenes/player/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+var can_laser: bool = true
+var can_grenade: bool = true
+
+signal laser(pos, direction)
+signal grenade(pos, direction)
+
+func _process(_delta: float) -> void:
+	
+	#input
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * 500
+	move_and_slide()
+	
+	# rotate
+	look_at(get_global_mouse_position())
+	var p_direction = (get_global_mouse_position() - position).normalized()
+
+
+	# laser shooting input
+	if Input.is_action_pressed("primary action") and can_laser:
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
+		can_laser = false
+		$LaserTimer.start()
+		laser.emit(selected_laser.global_position, p_direction)
+		
+	if Input.is_action_pressed("secondary action") and can_grenade:
+		var pos = $LaserStartPositions.get_children()[0].global_position
+		can_grenade = false
+		$GrenadeTimer.start()
+		grenade.emit(pos, p_direction)
+		
+
+func _on_laser_timer_timeout() -> void:
+	can_laser = true
+
+func _on_grenade_timer_timeout() -> void:
+	can_grenade = true 
